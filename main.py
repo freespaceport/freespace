51e13a94cdafc4cce2eba055d98b70fa73b95bc0
bogdann1 <bogdan.jordan.mod@gmail.com>
2025-01-31 20:21:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    pass
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
